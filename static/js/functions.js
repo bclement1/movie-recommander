88f447fa0dbf1f91a05ea0da57d57c89a1655ee3
Clément Boulay <clement.boulay@student-cs.fr>
2023-01-06 10:55:09 +0100

    Change form UI, add multiple questions, still need to handle multiple ticks display

--- static/js/functions.js
+++ static/js/functions.js
@@ -3,6 +3,8 @@
 function selectOnlyThis(id, begin, end) {
     for (var i = begin; i <= end; i++) {
         document.getElementById(i).checked = false;
+        document.getElementById("tick" + i.toString()).style.display = "none";
     }
     document.getElementById(id).checked = true;
+    document.getElementById("tick" + id.toString()).style.display = "block";
 }
