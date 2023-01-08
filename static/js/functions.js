1a39a7a1b1815a8a7a0ef73623b687061a6bd4b2
Clément Boulay <clement.boulay@student-cs.fr>
2023-01-08 13:19:19 +0100

    Add a red border around unanswered questions when submitting

--- static/js/functions.js
+++ static/js/functions.js
@@ -25,9 +25,6 @@ function updateRequirements(elementTicked) {
     var numChoices = choices.length;
 
     for (var i = 0; i < numChoices; i++) {
-        // logs for debug
-        console.log("Element fetched:", choices[i].children[0].children[1]);
-
         // mark all available choices in the question as not required (they initially are)
         choices[i].children[0].children[1].required = false;
     }
@@ -44,4 +41,27 @@ function updateRequirements(elementTicked) {
 function checkSubmission(form) {
     // check that the form is correctly completed before submitting
     
+    // get all question blocks
+    var questionBlocks = form.parentElement.children; // last element of this list is the form's button
+    var numQuestionBlocks = questionBlocks.length;
+
+    for (var i = 0; i < numQuestionBlocks - 1; i++) { // one question block
+        // make the border of the question block gray
+        var currentQuestions = questionBlocks[i].children[1]; // ul
+        currentQuestions.style.border = "1px solid grey";
+        console.log("Question block selected:", currentQuestions);
+        
+
+        // check if the question block is properly completed
+        var possibleAnswers = currentQuestions.children; // list of li
+        var numPossibleAnswers = possibleAnswers.length;
+        
+        for (var j = 0; j < numPossibleAnswers; j++) {
+            var currentAnswer = possibleAnswers[j].children[0].children[1]; // input block
+            if ((currentAnswer.required) && (!(currentAnswer.checked))) {
+                currentQuestions.style.border = "1px solid red";
+            }
+        }
+    }
+
 }
