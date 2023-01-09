3a9966cd4ff1e1d39e8fd94a167a98ca159b1ff3
noblotni <noblotni@gmail.com>
2023-01-09 22:45:23 +0100

    QUeries take into account actor and duration.

--- server_flask.py
+++ server_flask.py
@@ -1,8 +1,8 @@
 """Flask server for movies recommendations."""
 from typing import Dict
 from flask import Flask, render_template, request
-from sparql_queries import category_query
 import json
+from sparql_queries import query_dbpedia
 
 app = Flask(__name__)
 
@@ -12,74 +12,34 @@ def home():
     """Handle website's home page."""
     if request.method == "POST":
         # user entered its preferences, query DBpedia and return the result
-        count = 1 # so far, only the first question is handled
-        for category in request.form.values():
-            if count > 1:
-                continue
-            query_res = category_query(category=category)
-            headings = ["Title", "Abstract"]
-            data = get_data_from_json(query_res)
-            count += 1
-            """Pour test - Clément (fais office de placeholder pour l'instant)"""
-            main_list = {
-                "data": 
-                [
-                {
-                    "title": "title1",
-                    "abstract": "abstract1",
-                    "img": None 
-                },
-                {
-                    "title": "nicolas",
-                    "abstract": "abstract2",
-                    "img": None 
-                },
-                {
-                    "title": "title3",
-                    "abstract": "abstract3",
-                    "img": None 
-                }
-                ]
-            }
-            reco_list1 = {
-                "data":
-                [
-                {
-                    "title": "title1",
-                    "abstract": "abstract1",
-                    "img": None 
-                },
-                {
-                    "title": "nicolas",
-                    "abstract": "abstract2",
-                    "img": None 
-                },
-                {
-                    "title": "title3",
-                    "abstract": "abstract3",
-                    "img": None 
-                }
-                ]
-            }
-            reco_list2 = {
-                "data": [
-                {
-                    "title": "title1",
-                    "abstract": "abstract1",
-                    "img": None 
-                },
-                {
-                    "title": "title2",
-                    "abstract": "abstract2",
-                    "img": None 
-                },
-                {
-                    "title": "title3",
-                    "abstract": "abstract3",
-                    "img": None 
-                }
-                ]
-            }
+        preferences = list(request.form.values())
+        query_res = query_dbpedia(
+            category=preferences[0], actor=preferences[1], time=preferences[2]
+        )
+        headings = ["Title", "Abstract"]
+        data = get_data_from_json(query_res)
+        """Pour test - Clément (fais office de placeholder pour l'instant)"""
+        main_list = {
+            "data": [
+                {"title": "title1", "abstract": "abstract1", "img": None},
+                {"title": "nicolas", "abstract": "abstract2", "img": None},
+                {"title": "title3", "abstract": "abstract3", "img": None},
+            ]
+        }
+        reco_list1 = {
+            "data": [
+                {"title": "title1", "abstract": "abstract1", "img": None},
+                {"title": "nicolas", "abstract": "abstract2", "img": None},
+                {"title": "title3", "abstract": "abstract3", "img": None},
+            ]
+        }
+        reco_list2 = {
+            "data": [
+                {"title": "title1", "abstract": "abstract1", "img": None},
+                {"title": "title2", "abstract": "abstract2", "img": None},
+                {"title": "title3", "abstract": "abstract3", "img": None},
+            ]
+        }
         return render_template(
             "answer.html",
             headings=headings,
@@ -87,7 +47,8 @@ def home():
             name="answer",
             main_list=json.dumps(main_list),
             reco_list1=json.dumps(reco_list1),
-            reco_list2=json.dumps(reco_list2))
+            reco_list2=json.dumps(reco_list2),
+        )
     # else, keep the app running
     return render_template("home.html", name="home")
 
@@ -95,11 +56,12 @@ def home():
 def get_data_from_json(json: Dict):
     """Parse the JSON file received from the query to DBpedia."""
     data = []
-    for result in json["results"]["bindings"]: # iterate over matching movies
+    for result in json["results"]["bindings"]:  # iterate over matching movies
         movie_abstract = result["abstract"]["value"]
         movie_title = result["name"]["value"]
         data.append([movie_title, movie_abstract])
     return data
 
+
 if __name__ == "__main__":
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(host="localhost", port=5000, debug=True)
--- sparql_queries.py
+++ sparql_queries.py
@@ -1,10 +1,11 @@
 """Generate and send SparQL queries to DBpedia API's SparQL endpoint."""
 from SPARQLWrapper import SPARQLWrapper, JSON
 
-def category_query(category: str):
+
+def query_dbpedia(category: str, actor: str, time: str):
     """Query DBpedia using SparQL and return the result (JSON format)."""
     sparql = SPARQLWrapper("https://dbpedia.org/sparql")
-    query_txt = generate_query_txt(category)
+    query_txt = generate_query_txt(category=category, actor=actor, time=time)
     print("SparQL query reads: \n{}".format(query_txt))
     sparql.setQuery(query_txt)
     sparql.setReturnFormat(JSON)
@@ -12,17 +13,29 @@ def category_query(category: str):
     return query_res
 
 
-def generate_query_txt(category: str):
+def generate_query_txt(category: str, actor: str, time: str):
     """Return a SparQL query generated using the movie category specified by the user."""
     category = category.replace("-", "_")
+    actor = actor.replace(" ", "_")
     # SELECT section
-    query_txt = "SELECT DISTINCT ?film, ?abstract, ?name"
+    query_txt = "SELECT DISTINCT ?film, ?abstract, ?name, ?run"
     # WHERE section
     query_txt += "\nWHERE\n{\n"
     query_txt += "?film dbo:wikiPageWikiLink dbr:{}_film .\n".format(category)
     query_txt += "?film rdfs:comment ?abstract .\n"
     query_txt += "?film dbp:name ?name . \n"
+    query_txt += "?film dbo:runtime ?run . \n"
+    query_txt += "?film dbo:starring dbr:{} . \n".format(actor)
+    # Duration filter
+    query_txt = query_time_filter(time=time, query_txt=query_txt)
     query_txt += "FILTER langMatches(lang(?abstract), 'en')\n"
     # End of WHERE section
-    query_txt += "} LIMIT 5"
+    query_txt += "} LIMIT 10"
+    return query_txt
+
+
+def query_time_filter(time: str, query_txt: str):
+    if time != "inf":
+        time_float = float(time)
+        query_txt += "FILTER( ?run <= {}) .\n".format(time_float * 3600)
     return query_txt
