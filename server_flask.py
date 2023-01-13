41fbd03b751ba656ac8422ddff2c02679638e46d
noblotni <noblotni@gmail.com>
2023-01-13 11:35:09 +0100

    Modify run time display.

--- server_flask.py
+++ server_flask.py
@@ -4,6 +4,10 @@ from flask import Flask, render_template, request
 import json
 from sparql_queries import query_dbpedia
 
+
+HOUR_TO_SECOND = 3600
+HOUR_TO_MINUTE = 60
+
 app = Flask(__name__)
 
 
@@ -15,7 +19,7 @@ def home():
         preferences = list(request.form.values())
         # Make queries
         query_pref = query_dbpedia(
-            category=preferences[0], actor=preferences[1], time=preferences[2]
+            category=preferences[0], actor=preferences[1], duration=preferences[2]
         )
         query_same_cat = query_dbpedia(category=preferences[0])
         query_same_actor = query_dbpedia(actor=preferences[1])
@@ -68,9 +72,19 @@ def get_data_from_json(json: Dict):
     for result in json["results"]["bindings"]:  # iterate over matching movies
         movie_abstract = result["abstract"]["value"]
         movie_title = result["name"]["value"]
-        movie_runtime = result["run"]["value"]
+        movie_runtime = convert_runtime(result["run"]["value"])
         data.append([movie_title, movie_abstract, movie_runtime])
     return data
+
+
+def convert_runtime(runtime: str):
+    """Convert runtime format to hours/minutes"""
+    # Run time in seconds
+    runtime_float = float(runtime)
+    hours = runtime_float // HOUR_TO_SECOND
+    minutes = (runtime_float % HOUR_TO_SECOND) // HOUR_TO_MINUTE
+    runtime = "{}h{}min".format(hours, minutes)
+    return runtime
 
 
 if __name__ == "__main__":
--- sparql_queries.py
+++ sparql_queries.py
@@ -7,10 +7,14 @@ HOUR_TO_SECOND = 3600
 logging.basicConfig(level=logging.INFO)
 
 
-def query_dbpedia(category: str, actor: str, time: str):
+def query_dbpedia(
+    category: Optional[str] = None,
+    actor: Optional[str] = None,
+    duration: Optional[str] = None,
+):
     """Query DBpedia using SparQL and return the result (JSON format)."""
     sparql = SPARQLWrapper("https://dbpedia.org/sparql")
-    query_txt = generate_query_txt(category=category, actor=actor, time=time)
+    query_txt = generate_query_txt(category=category, actor=actor, duration=duration)
     logging.info("SparQL query reads: \n{}".format(query_txt))
     sparql.setQuery(query_txt)
     sparql.setReturnFormat(JSON)
@@ -21,10 +25,10 @@ def query_dbpedia(category: str, actor: str, time: str):
 def generate_query_txt(
     category: Optional[str] = None,
     actor: Optional[str] = None,
-    time: Optional[str] = None,
+    duration: Optional[str] = None,
 ):
     """Return a SparQL query generated using the fields specified by the user."""
-    if not category and not actor and not time:
+    if not category and not actor and not duration:
         logging.error("No argument specified. Can not generate SparQL query.")
         raise ValueError
     # SELECT section
@@ -42,8 +46,8 @@ def generate_query_txt(
         actor = actor.replace(" ", "_")
         query_txt += "?film dbo:starring dbr:{} . \n".format(actor)
     # Duration filter
-    if time:
-        query_txt = query_time_filter(time=time, query_txt=query_txt)
+    if duration:
+        query_txt = query_time_filter(duration=duration, query_txt=query_txt)
     # Language filter
     query_txt += "FILTER langMatches(lang(?abstract), 'en')\n"
     # End of WHERE section
@@ -51,8 +55,8 @@ def generate_query_txt(
     return query_txt
 
 
-def query_time_filter(time: str, query_txt: str):
-    if time != "inf":
-        time_float = float(time)
-        query_txt += "FILTER( ?run <= {}) .\n".format(time_float * HOUR_TO_SECOND)
+def query_time_filter(duration: str, query_txt: str):
+    if duration != "inf":
+        duration_float = float(duration)
+        query_txt += "FILTER( ?run <= {}) .\n".format(duration_float * HOUR_TO_SECOND)
     return query_txt
